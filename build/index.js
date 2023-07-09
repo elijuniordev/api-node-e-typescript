aa138260da5c5ec00647c66bf5645a08f1dd54b9
Eli Junior <juusoares99@gmail.com>
2023-07-09 17:14:02 -0300

    feat: adicionado seed de cidades

--- build/index.js
+++ build/index.js
@@ -1,7 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Server_1 = require("./server/Server");
-Server_1.server.listen(process.env.PORT || 9001, () => {
-    console.log(`App rodando na porta ${process.env.PORT || 9001} `);
-});
+const knex_1 = require("./server/database/knex");
+const startServer = () => {
+    Server_1.server.listen(process.env.PORT || 9001, () => {
+        console.log(`App rodando na porta ${process.env.PORT || 9001} `);
+    });
+};
+if (process.env.IS_LOCALHOST !== 'true') {
+    knex_1.Knex.migrate
+        .latest()
+        .then(() => {
+        startServer();
+    })
+        .catch(console.log);
+}
+else {
+    startServer();
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import { server } from './server/Server';
+import { Knex } from './server/database/knex';
 
-server.listen(process.env.PORT || 9001, () => {
-  console.log(`App rodando na porta ${process.env.PORT || 9001} `);
-});
+const startServer = () => {
+  server.listen(process.env.PORT || 9001, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 9001} `);
+  });
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate
+    .latest()
+    .then(() => {
+      Knex.seed
+        .run()
+        .then(() => startServer())
+        .catch(console.log);
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
