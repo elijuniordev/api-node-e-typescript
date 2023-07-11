ae87497836c5000ca6c1410e3cb7f8a4c90a1a29
Eli Junior <juusoares99@gmail.com>
2023-07-11 18:18:04 -0300

    feat: refatorado os testes para autenticação

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -35,5 +35,21 @@ export const test: Knex.Config = {
 };
 
 export const production: Knex.Config = {
-  ...development,
+  client: 'pg',
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,6 +1,8 @@
 import supertest from 'supertest';
-import { server } from '../src/server/Server';
 import { Knex } from '../src/server/database/knex';
+
+import { server } from '../src/server/Server';
+
 
 beforeAll(async () => {
   await Knex.migrate.latest();
@@ -10,5 +12,6 @@ beforeAll(async () => {
 afterAll(async () => {
   await Knex.destroy();
 });
+
 
 export const testServer = supertest(server);
